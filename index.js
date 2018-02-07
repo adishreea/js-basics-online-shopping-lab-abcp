0700b8e28c991e6c7dedae9cf6ececc843b6f0ca
Adishree A <adishree.annabathula@gmail.com>
2018-02-07 15:38:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,26 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  var itemObj = {item: Math.random()}
+  itemObj = cart
+  return "${item} has been added to your cart."
 }
 
 function viewCart() {
   // write your code here
+  console.log("In your cart, you have")
+  for (var i = 0; i < cart.length; i++){
+    console.log("${cart.key} at ${cart.value}")
+  }
 }
 
 function total() {
   // write your code here
+  var sum = 0
+  for (var i = 0; i < cart.length; i++){
+    sum += Object.keys(cart)[i]
+  }
+  return sum
 }
 
 function removeFromCart(item) {
